2ed0813368204321b157ed58cc1ea52fe5be12cb
Roy Larsen <roy.larsen@mobileheartbeat.com>
2022-06-21 11:39:39 -0400

    Moving code to the src/ folder. Adding pyproject.toml to allow package to be installed via pip

---
+++ src/ghapi/ghapi.py
@@ -0,0 +1,26 @@
+from logging import exception
+import requests, json, urllib3
+
+class GHAPI:
+    def __init__(self, ghtoken):
+        self.ghurl = "https://api.github.com/repos/hca-mobilehb/"
+        self.ghtoken = ghtoken
+
+    def get_prs(self, repos):
+        headers = {
+            "authorization" : "bearer " + self.ghtoken,
+            "Accept": "application/vnd.github.v3+json"
+        }
+
+        resp = {}
+        prs = []
+        for repo in repos.split(","):
+            resp[repo] = []
+            r = requests.get("{0}{1}/pulls".format(self.ghurl, repo), headers=headers)
+            for pull in r.json():
+                for labels in pull["labels"]:
+                    if "infrared" in labels["name"]:
+                        prs.append({pull['html_url'] : [pull['title'], pull['user']['login']]})
+                resp[repo] = prs
+            prs = []
+        return resp
---
+++ src/main.py
@@ -0,0 +1,51 @@
+import click, os
+from ghapi.ghapi import GHAPI
+
+@click.group("bd")
+def cli():
+    pass
+
+@cli.group("config")
+def config():
+    """NOT IMPLEMENTED
+    Sub-commands related to configuring the bd tool
+    """
+    pass
+
+@cli.group("deploy")
+def deploy():
+    """NOT IMPLEMENTED
+    Sub-commands involved in deployments
+    \n\r\t With Great Power Comes Great Responsibity 
+    \n\t\t- Uncle Ben 
+    \n\r\t\t\t- Roy Larsen
+    """
+    pass
+
+@cli.group("gh")
+def gh():
+    """Sub-commands involved in retrieving information from GitHub"""
+    if os.environ.get("GITHUB_TOKEN") is None:
+        print("Please make sure to set the GITHUB_TOKEN environment variable with your GitHubPAT")
+    pass
+
+@gh.command()
+@click.option("-r", "--repos", default="mh-iac,banyan-deployments", help="Comma separated list of repositories to get PRs from. By default, mh-iac and banyan-deployments")
+@click.option("-f", "--format", is_flag=True, default=False, help="Formats the output for posting on Slack")
+def get_prs(repos, format):
+    """Gets PRs based on rules"""
+    if os.environ.get("GITHUB_TOKEN") is not None:
+        token = os.environ.get("GITHUB_TOKEN")
+    gh = GHAPI(ghtoken=token)
+
+    if format:
+        for repo, prs in gh.get_prs(repos).items():
+            print("*{0}*".format(repo))
+            for pr in prs:
+                for url, details in pr.items():
+                    print(" * {0} \n\t* {1} \n\t* {2}".format(url, details[0], details[1]))
+    else:
+        print(gh.get_prs(repos))
+
+if __name__ == "__main__":
+    cli()
