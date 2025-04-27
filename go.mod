cf5830235efaf1145e8019da0f8b0a79f853331e
Roy Larsen <roy.larsen@mobileheartbeat.com>
2025-04-26 21:51:29 -0500

    moving to urfave/cli

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module main
 
 go 1.22.9
 
-require github.com/alecthomas/kong v1.10.0 // indirect
+require (
+	github.com/alecthomas/kong v1.10.0 // indirect
+	github.com/urfave/cli/v3 v3.2.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,26 +1,43 @@
 package main
 
-import "github.com/alecthomas/kong"
+import (
+  "fmt"
+  "log"
+  "context"
+  "os"
 
-var CLI struct {
-  Rm struct {
-    Force     bool `help:"Force removal."`
-    Recursive bool `help:"Recursively remove files."`
+  "github.com/urfave/cli/v3"
+)
 
-    Paths []string `arg:"" name:"path" help:"Paths to remove." type:"path"`
-  } `cmd:"" help:"Remove files."`
-
-  Ls struct {
-    Paths []string `arg:"" optional:"" name:"path" help:"Paths to list." type:"path"`
-  } `cmd:"" help:"List paths."`
+func getprs() {
+  fmt.Println("get-prs function!")
 }
 
 func main() {
-  ctx := kong.Parse(&CLI)
-  switch ctx.Command() {
-  case "rm <path>":
-  case "ls":
-  default:
-    panic(ctx.Command())
-  }
+    cmd := &cli.Command{
+            Commands: []*cli.Command{
+                {
+                  Name:    "github",
+                  Aliases: []string{"gh"},
+                  Usage:   "tasks related to github",
+                  Commands: []*cli.Command{
+                    {
+                      Name: "get-prs",
+                      Aliases: []string{"prs"},
+                      Usage: "Gets a list of open PRs",
+                      Flags: []cli.Flag{
+                        &cli.BoolFlag{Name: "format", Aliases: []string{"f"}},
+                      },
+                      Action: func (ctx context.Context, cmd *cli.Command) error{
+                        getprs()
+                        return nil
+                      },
+                    },
+                  },
+                },
+            },
+          }
+    if err := cmd.Run(context.Background(), os.Args); err != nil {
+        log.Fatal(err)
+    }
 }
